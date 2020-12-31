00f6b553a9c02ff086fe7a6c3130dcdcccffca5c
Bohdlesk <bohdanleskovets@gmail.com>
2020-12-31 14:57:53 +0200

    added DB confection

---
+++ src/db.ts
@@ -0,0 +1,18 @@
+// main methods (CRUD) + db connection; imports stuff from models.ts.
+
+const pg = require('pg');
+import * as consts from './const';
+
+const client = new pg.Client(consts.conString);
+
+client.connect(function (err) {
+    if (err) {
+        return console.error('could not connect to postgres', err);
+    }
+    client.query('SELECT NOW() AS "theTime"', function (err, result) {
+        if (err) {
+            return console.error('error running query', err);
+        }
+        console.log(result.rows[0].theTime);
+    });
+});
